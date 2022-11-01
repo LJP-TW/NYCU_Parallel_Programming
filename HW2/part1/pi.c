ac105ec43e0d0983cd5641c36da0e61aa54c4159
LJP-TW <accr94238@gmail.com>
2022-11-02 00:54:54 +0800

    HW2/part1: vectorize tf_estimate_pi

--- HW2/part1/pi.c
+++ HW2/part1/pi.c
@@ -7,6 +7,18 @@
 
 #define U32_MAX 0xffffffff
 
+typedef long int v4di __attribute__ ((vector_size (32)));
+typedef union {
+    v4di v;
+    long int e[4];
+} ve4di;
+
+typedef double v4df __attribute__ ((vector_size (32)));
+typedef union {
+    v4df v;
+    double e[4];
+} ve4df;
+
 typedef unsigned long u64;
 typedef unsigned int u32;
 typedef long long int s64;
@@ -49,15 +61,60 @@ void add_hit(int value)
     pthread_mutex_unlock(&hit_mutex);
 }
 
+s64 _tf_estimate_pi_4(s64 toss_cnt, prng_state *prng)
+{
+    s64 _hit = 0;
+    ve4df fone = {1, 1, 1, 1};
+    ve4di ione = {1, 1, 1, 1};
+    ve4di result = {0, 0, 0, 0};
+    
+    for (s64 i = 0; i < toss_cnt; i+=4) {
+        ve4df distance;
+        ve4di cmp;
+        
+        ve4df x = { get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1) };
+        ve4df y = { get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1),
+                    get_rand(prng, -1, 1) };
+        distance.v = x.v * x.v + y.v * y.v;
+
+        cmp.v = distance.v <= fone.v;
+        result.v = result.v + (ione.v & cmp.v);
+
+        // printf("x: %f, %f, %f, %f\n", x.e[0], x.e[1], x.e[2], x.e[3]);
+        // printf("y: %f, %f, %f, %f\n", y.e[0], y.e[1], y.e[2], y.e[3]);
+        // printf("d: %f, %f, %f, %f\n", distance.e[0], distance.e[1], distance.e[2], distance.e[3]);
+        // printf("c: %ld, %ld, %ld, %ld\n", cmp.e[0], cmp.e[1], cmp.e[2], cmp.e[3]);
+        // printf("r: %ld, %ld, %ld, %ld\n", result.e[0], result.e[1], result.e[2], result.e[3]);
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        _hit += result.e[i];
+    }
+
+    // printf("_hit: %lld\n", _hit);
+
+    return _hit;
+}
+
 void *tf_estimate_pi(void *_toss_cnt)
 {
     s64 toss_cnt = (s64)_toss_cnt;
     s64 _hit = 0;
+    s64 remain;
     prng_state prng;
 
     rand_init(&prng);
     
-    for (s64 i = 0; i < toss_cnt; i++) {
+    remain = toss_cnt % 4;
+
+    _hit += _tf_estimate_pi_4(toss_cnt - remain, &prng);
+
+    for (s64 i = 0; i < remain; i++) {
         double x = get_rand(&prng, -1, 1);
         double y = get_rand(&prng, -1, 1);
         double distance = x * x + y * y;
