904893e61a3990b1c1b196c02caea501bf1bad06
LJP-TW <accr94238@gmail.com>
2022-11-15 15:11:37 +0800

    HW3/part2: BFS first commit

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -33,6 +33,7 @@ void top_down_step(
     vertex_set *new_frontier,
     int *distances)
 {
+    #pragma omp parallel for
     for (int i = 0; i < frontier->count; i++)
     {
 
@@ -51,7 +52,7 @@ void top_down_step(
             if (distances[outgoing] == NOT_VISITED_MARKER)
             {
                 distances[outgoing] = distances[node] + 1;
-                int index = new_frontier->count++;
+                int index = __sync_fetch_and_add(&new_frontier->count, 1);
                 new_frontier->vertices[index] = outgoing;
             }
         }
@@ -104,6 +105,40 @@ void bfs_top_down(Graph graph, solution *sol)
     }
 }
 
+int bottom_up_step(
+    Graph g,
+    int layer,
+    int *distances)
+{
+    int updated = 0;
+
+    #pragma omp parallel for (schedule)
+    for (int v = 0; v < g->num_nodes; ++v)
+    {
+        if (distances[v] != NOT_VISITED_MARKER)
+            continue;
+
+        int start_edge = g->incoming_starts[v];
+        int end_edge = (v == g->num_nodes - 1)
+                       ? g->num_edges
+                       : g->incoming_starts[v + 1];
+
+        for (int edgeidx = start_edge; edgeidx < end_edge; ++edgeidx)
+        {
+            int parent_v = g->incoming_edges[edgeidx];
+
+            if (distances[parent_v] == layer)
+            {
+                updated = 1;
+                distances[v] = layer + 1;
+                break;
+            }
+        }
+    }
+
+    return updated;
+}
+
 void bfs_bottom_up(Graph graph, solution *sol)
 {
     // For PP students:
@@ -117,6 +152,19 @@ void bfs_bottom_up(Graph graph, solution *sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+    // initialize all nodes to NOT_VISITED
+    int layer = 0;
+
+    #pragma omp parallel for
+    for (int i = 0; i < graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (bottom_up_step(graph, layer, sol->distances))
+    {
+        ++layer;
+    }
 }
 
 void bfs_hybrid(Graph graph, solution *sol)
@@ -125,4 +173,40 @@ void bfs_hybrid(Graph graph, solution *sol)
     //
     // You will need to implement the "hybrid" BFS here as
     // described in the handout.
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set *frontier = &list1;
+    vertex_set *new_frontier = &list2;
+
+    int layer = 0;
+
+    // initialize all nodes to NOT_VISITED
+    for (int i = 0; i < graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count != 0 && layer < 2)
+    {
+        vertex_set_clear(new_frontier);
+
+        top_down_step(graph, frontier, new_frontier, sol->distances);
+
+        // swap pointers
+        vertex_set *tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+
+        ++layer;
+    }
+
+    while (bottom_up_step(graph, layer, sol->distances))
+    {
+        ++layer;
+    }
 }
---
+++ HW3/part2/graphs/README.md
@@ -0,0 +1,3 @@
+http://sslab.cs.nctu.edu.tw/~acliu/all_graphs.tgz
+
+* Be careful, this is a 3GB download
