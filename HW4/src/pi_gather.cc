3616b977cc4129720769a1f8fb1cc7f2147a83f0
LJP-TW <accr94238@gmail.com>
2022-11-22 20:15:15 +0800

    HW4: pi_gather done

--- HW4/src/pi_gather.cc
+++ HW4/src/pi_gather.cc
@@ -5,6 +5,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#pragma GCC optimize ("O3")
+
+long long int hit;
+
+double rand_get(double min, double max, unsigned int *seed)
+{
+    int r = rand_r(seed);
+    return min + ((double)(r) / RAND_MAX) * (max - min);
+}
+
+void tf_estimate_pi(long long int tosses_cnt, int rank)
+{
+    unsigned int seed = getpid() ^ time(NULL) ^ (0xaed01498 << rank);
+
+    for (long long int toss = 0; toss < tosses_cnt; toss ++) {
+        double x, y, distance_squared;
+        x = rand_get(-1, 1, &seed);
+        y = rand_get(-1, 1, &seed);
+        distance_squared = x * x + y * y;
+        if (distance_squared <= 1)
+            hit++;
+    }
+}
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -16,12 +40,29 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     // TODO: use MPI_Gather
+    long long int part_tosses;
+    long long int hits[world_size];
 
-    if (world_rank == 0)
-    {
+    part_tosses = tosses / world_size;
+    if (world_rank == 0) {
+        part_tosses += (tosses % world_size);
+    }
+
+    tf_estimate_pi(part_tosses, world_rank);
+
+    MPI_Gather(&hit, 1, MPI_LONG_LONG, hits, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        for (int i = 1; i < world_size; ++i) {
+            hit += hits[i];
+        }
+
         // TODO: PI result
+        pi_result = 4 * hit / ((double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
