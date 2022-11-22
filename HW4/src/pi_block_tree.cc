8ad482cdd32dd473c48fdb0d6fdf61bcc3af43a3
LJP-TW <accr94238@gmail.com>
2022-11-22 18:46:42 +0800

    HW4: pi_block_tree done

--- HW4/src/pi_block_tree.cc
+++ HW4/src/pi_block_tree.cc
@@ -5,6 +5,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#pragma GCC optimize ("O3")
+
+long long int hit;
+
+double rand_get(double min, double max, unsigned int *seed)
+{
+    int r = rand_r(seed);
+    return min + ((double)(r) / RAND_MAX) * (max - min);
+}
+
+void tf_estimate_pi(long long int tosses_cnt, int rank)
+{
+    unsigned int seed = getpid() ^ time(NULL) ^ (0xaed01498 << rank);
+
+    for (long long int toss = 0; toss < tosses_cnt; toss ++) {
+        double x, y, distance_squared;
+        x = rand_get(-1, 1, &seed);
+        y = rand_get(-1, 1, &seed);
+        distance_squared = x * x + y * y;
+        if (distance_squared <= 1)
+            hit++;
+    }
+}
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -15,13 +39,61 @@ int main(int argc, char **argv)
     int world_rank, world_size;
     // ---
 
+    long long int part_tosses;
+    int is_buddy_master;
+    int buddy_rank;
+    int buddy_layer = 0;
+
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     // TODO: binary tree redunction
+    part_tosses = tosses / world_size;
+    if (world_rank == 0) {
+        part_tosses += (tosses % world_size);
+    }
 
+    tf_estimate_pi(part_tosses, world_rank);
+
+    world_size >>= 1;
+
+    while (world_size) {
+        buddy_rank = world_rank ^ (1 << buddy_layer);
+        is_buddy_master = !(world_rank & (1 << buddy_layer));
+
+        if (is_buddy_master) {
+            // Recv
+            long long int holder;
+
+            MPI_Recv(&holder,
+                     1,
+                     MPI_LONG_LONG,
+                     buddy_rank,
+                     0,
+                     MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+
+            hit += holder;
+        } else {
+            // Send
+            MPI_Send(&hit,
+                     1,
+                     MPI_LONG_LONG,
+                     buddy_rank,
+                     0,
+                     MPI_COMM_WORLD);
+            break;
+        }
+
+        ++buddy_layer;
+        world_size >>= 1;
+    }
+    
     if (world_rank == 0)
     {
         // TODO: PI result
+        pi_result = 4 * hit / ((double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
--- HW4/src/test.py
+++ HW4/src/test.py
@@ -11,11 +11,11 @@ def test_mpi_hello(np=8):
         report.write('\n')
         report.flush()
 
-def test_pi_block_linear(np=8, pi_block_linear='pi_block_linear'):
-    cmd = ['mpirun', '-np', str(np), '--hostfile', 'hosts_pi_block_linear.txt', pi_block_linear, '1000000000']
+def test_pi(np=8, pi_block_linear='pi_block_linear'):
+    cmd = ['mpirun', '-np', str(np), '--hostfile', 'hosts_part1.txt', pi_block_linear, '1000000000']
 
     with open('report.txt', "a") as report:
-        report.write('mpirun -np {} --hostfile hosts_pi_block_linear.txt {} 1000000000\n'.format(np, pi_block_linear))
+        report.write('mpirun -np {} --hostfile hosts_part1.txt {} 1000000000\n'.format(np, pi_block_linear))
         report.flush()
         subprocess.run(cmd, stdout=report)
         report.write('\n')
@@ -27,13 +27,22 @@ with open('report.txt', "w") as report:
 test_mpi_hello(8)
 test_mpi_hello(10)
 
-test_pi_block_linear(2, 'pi_block_linear')
-test_pi_block_linear(2, '/HW4/ref/pi_block_linear')
-test_pi_block_linear(4, 'pi_block_linear')
-test_pi_block_linear(4, '/HW4/ref/pi_block_linear')
-test_pi_block_linear(8, 'pi_block_linear')
-test_pi_block_linear(8, '/HW4/ref/pi_block_linear')
-test_pi_block_linear(12, 'pi_block_linear')
-test_pi_block_linear(12, '/HW4/ref/pi_block_linear')
-test_pi_block_linear(16, 'pi_block_linear')
-test_pi_block_linear(16, '/HW4/ref/pi_block_linear')
+test_pi(2, 'pi_block_linear')
+test_pi(2, '/HW4/ref/pi_block_linear')
+test_pi(4, 'pi_block_linear')
+test_pi(4, '/HW4/ref/pi_block_linear')
+test_pi(8, 'pi_block_linear')
+test_pi(8, '/HW4/ref/pi_block_linear')
+test_pi(12, 'pi_block_linear')
+test_pi(12, '/HW4/ref/pi_block_linear')
+test_pi(16, 'pi_block_linear')
+test_pi(16, '/HW4/ref/pi_block_linear')
+
+test_pi(2, 'pi_block_tree')
+test_pi(2, '/HW4/ref/pi_block_tree')
+test_pi(4, 'pi_block_tree')
+test_pi(4, '/HW4/ref/pi_block_tree')
+test_pi(8, 'pi_block_tree')
+test_pi(8, '/HW4/ref/pi_block_tree')
+test_pi(16, 'pi_block_tree')
+test_pi(16, '/HW4/ref/pi_block_tree')
