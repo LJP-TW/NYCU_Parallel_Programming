d0a4261fa9fb58378105dfd820f48fd92e4dac2f
LJP-TW <accr94238@gmail.com>
2022-11-22 17:18:27 +0800

    HW4: pi_block_linear done

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -5,6 +5,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#pragma GCC optimize ("O3")
+
+long long int hit;
+
+double rand_get(double min, double max, unsigned int *seed)
+{
+    int r = rand_r(seed);
+    return min + ((double)(r) / RAND_MAX) * (max - min);
+}
+
+void tf_estimate_pi(long long int tosses_cnt, int rank)
+{
+    unsigned int seed = getpid() ^ time(NULL) ^ (0xaed01498 << rank);
+
+    for (long long int toss = 0; toss < tosses_cnt; toss ++) {
+        double x, y, distance_squared;
+        x = rand_get(-1, 1, &seed);
+        y = rand_get(-1, 1, &seed);
+        distance_squared = x * x + y * y;
+        if (distance_squared <= 1)
+            hit++;
+    }
+}
+
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -16,19 +40,49 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     if (world_rank > 0)
     {
         // TODO: handle workers
+        tosses /= world_size;
+        tf_estimate_pi(tosses, world_rank);
+
+        MPI_Send(&hit,
+                 1,
+                 MPI_LONG_LONG,
+                 0,
+                 0,
+                 MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: master
+        long long int part_tosses;
+
+        part_tosses = (tosses / world_size) + (tosses % world_size);
+        tf_estimate_pi(part_tosses, world_rank);
+
+        for (int i = 1; i < world_size; ++i) {
+            long long int holder;
+
+            MPI_Recv(&holder,
+                     1,
+                     MPI_LONG_LONG,
+                     i,
+                     0,
+                     MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+
+            hit += holder;
+        }
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
+        pi_result = 4 * hit / ((double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
--- HW4/src/test.py
+++ HW4/src/test.py
@@ -11,8 +11,29 @@ def test_mpi_hello(np=8):
         report.write('\n')
         report.flush()
 
+def test_pi_block_linear(np=8, pi_block_linear='pi_block_linear'):
+    cmd = ['mpirun', '-np', str(np), '--hostfile', 'hosts_pi_block_linear.txt', pi_block_linear, '1000000000']
+
+    with open('report.txt', "a") as report:
+        report.write('mpirun -np {} --hostfile hosts_pi_block_linear.txt {} 1000000000\n'.format(np, pi_block_linear))
+        report.flush()
+        subprocess.run(cmd, stdout=report)
+        report.write('\n')
+        report.flush()
+
 with open('report.txt', "w") as report:
     report.write('')
 
 test_mpi_hello(8)
 test_mpi_hello(10)
+
+test_pi_block_linear(2, 'pi_block_linear')
+test_pi_block_linear(2, '/HW4/ref/pi_block_linear')
+test_pi_block_linear(4, 'pi_block_linear')
+test_pi_block_linear(4, '/HW4/ref/pi_block_linear')
+test_pi_block_linear(8, 'pi_block_linear')
+test_pi_block_linear(8, '/HW4/ref/pi_block_linear')
+test_pi_block_linear(12, 'pi_block_linear')
+test_pi_block_linear(12, '/HW4/ref/pi_block_linear')
+test_pi_block_linear(16, 'pi_block_linear')
+test_pi_block_linear(16, '/HW4/ref/pi_block_linear')
